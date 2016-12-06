59b5874746d0eeb13da66dbca9edefa6d97f759c
WUYIMIN <wuyimin@oneapm.com>
2016-12-06 17:57:09 +0800

    remove unused moz sys.path and add graphviz & plantuml extension

--- docs/conf.py
+++ docs/conf.py
@@ -5,13 +5,14 @@ import sys
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 ROOT = os.path.normpath(os.path.join(HERE, '..'))
-sys.path.insert(0, os.path.join(ROOT, 'pylib', 'mozautomation'))
-sys.path.insert(0, os.path.join(ROOT, 'pylib', 'mozhg'))
-sys.path.insert(0, os.path.join(ROOT, 'pylib', 'reviewboardmods'))
+
+sys.path.append(os.path.abspath('exts'))
 
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
+    'sphinx.ext.graphviz',
+    'sphinxcontrib.plantuml',
 ]
 
 templates_path = ['_templates']
--- docs/installing/architecture.rst
+++ docs/installing/architecture.rst
@@ -8,8 +8,22 @@ Architecture
 系统架构
 --------------
 
+.. graphviz::
 
+   digraph {
+      "From" -> "To";
+   }
 
 
 数据流图
 --------------
+
+.. uml:: 
+   
+   @startuml
+   user -> (use PlantUML)
+
+   note left of user
+      Hello!   
+   end note
+   @enduml
